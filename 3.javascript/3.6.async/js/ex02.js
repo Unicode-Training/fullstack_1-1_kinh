ee5709aa1098239583a68860175dea68c8c14e6b
Hoang An Unicode <hoangan.web@gmail.com>
2026-05-10 10:06:03 +0700

    Add HTML and JavaScript files for button interaction functionality

---
+++ 3.javascript/3.6.async/js/ex02.js
@@ -0,0 +1,6 @@
+/*
+Yêu cầu: 
+- Khi click vào "Button 1" --> Đổi text "Giai đoạn 1", enable "Button 2"
+- Khi click vào "Button 2" --> Đổi text "Giai đoạn 2", enable "Button 3",
+- Khi click vào "Button 3" --> Đổi text "Giai đoạn 3"
+*/
